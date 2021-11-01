e4168c35afab8bc4ef419dbe9fca3efda2674024
Aditya Vernekar <adityaditya711@gmail.com>
2021-11-01 17:53:24 +0530

    user authentication

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,5 +18,6 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }});
-
-    module.exports = mongoose.model('User', UserSchema);
+    const User = mongoose.model('User', UserSchema);
+    User.createIndexes();
+    module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User'); 
+const {Schema} = require('mongoose');
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
-    obj = {
-        title: 'Login',
-        message: 'login to add',
-        error: null
+router.post('/',[
+    body('email').isEmail().withMessage("enter a valid email"),
+    body('password').isLength({ min: 8 }).withMessage('alteast 8 characters'),
+    body('name').isLength({ min: 3 }).withMessage('alteast 3 characters'),
+
+   
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj);
+    const {email,password,name} = req.body;
+    User.findOne({email}).then(user=>{
+        if(user){
+            return res.status(400).json({msg:"user already exists"});
+        }
+        const newUser = new User({
+            name,
+            email,
+            password
+        });
+        newUser.save().then(user=>{
+            res.json(user);
+        }).catch(err=>console.log(err));
+    }).catch(err=>console.log(err));
+
+    // res.send('user created');
+
+    
 })
 module.exports = router;
