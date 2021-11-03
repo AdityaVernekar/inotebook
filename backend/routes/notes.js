dd882693683569b69e27dbed4382b60a498143f9
Aditya Vernekar <adityaditya711@gmail.com>
2021-11-03 09:57:43 +0530

    endpoints completed

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -37,24 +37,40 @@ router.get('/fetchallnotes',fetchuser,(req,res)=>{
       })
    })
    // ROUTE 3 :update note :PUT /notes/:id
-   router.put('/updatenote/:id',fetchuser,[
-      body('title','enter a valid title ').not().isEmpty().isLength({min:3}),
-      body('description','enter a valid body').isLength({min:5})
-   ],(req,res)=>{
-      const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-      Notes.findOneAndUpdate({_id:req.params.id,user:req.user._id},{
+   router.put('/updatenote/:id',fetchuser, async (req,res)=>{
+
+      let note = await Notes.findById(req.params.id);
+      if(!note){
+         return res.status(404).send('note not found');
+      }
+      if(note.user.toString()!==req.user._id.toString()){
+         return res.status(401).send('unauthorized');
+      }
+
+      const updatenote = await Notes.findByIdAndUpdate(req.params.id,{
          title:req.body.title,
          description:req.body.description,
          tag:req.body.tag
-      })
-      .then(note=>{
-         res.send(note);
-      })
-      .catch(err=>{
-         res.json(err);
-      })
+      },{new:true})
+      res.send(updatenote);
+   })
+   // ROUTE 4 :delete note :DELETE /notes/:id
+   router.delete('/deletenote/:id',fetchuser, async (req,res)=>{
+      try {
+         let note = await Notes.findById(req.params.id);
+      if(!note){
+         return res.status(404).send('note not found');
+      }
+      if(note.user.toString()!==req.user._id.toString()){
+         return res.status(401).send('unauthorized');
+      }
+       const deletednote = await Notes.findByIdAndDelete(req.params.id);
+        
+      res.json({"success":"Note deleted successfully"});
+      } catch (error) {
+         console.log(error);
+         res.status(500).send('server error');
+
+      }
    })
 module.exports = router;
