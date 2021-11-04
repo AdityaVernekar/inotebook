df7399f0d17a01608dab1e83b71fda6b43cf2a0e
Aditya Vernekar <adityaditya711@gmail.com>
2021-11-04 11:12:15 +0530

    notes module

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <>
-    <NoteState>
-
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-        </Switch>
-      </Router>
-    </NoteState>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert message="you are an asshole bruhhh!" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
     </>
   );
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,31 +1,48 @@
-
-import { useState } from 'react';
-import NoteContext from './noteContext';
+import { useState } from "react";
+import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "adi",
-        "description": "3a",
-    }
-    const [state, setstate] = useState(s1);
-    const update = ()=>{
-       
-       setTimeout(()=>{
-        setstate({
-            "name": "padi",
-            "description": "9a",
-        })
-       },1000)
-       
-      
-    }
-    return(
-        <NoteContext.Provider value={{state,update}}>
-        {props.children}
-    </NoteContext.Provider>
-    )
-    
-}
+  const notesInitial = [
+    {
+      _id: "61812399b23540c39ce98f296",
+      user: "618234f53a617ff2cd34e7dd",
+      title: "first note",
+      description: "bro pehlawala hai",
+      tag: "sweet",
+      date: "2021-11-03T07:26:19.015Z",
+      __v: 0,
+    },
+    {
+      _id: "6182399b23540c319ce98f298",
+      user: "618234f53a617ff2cd34e7dd",
+      title: "first note",
+      description: "bro pehlawala hai",
+      tag: "sweet",
+      date: "2021-11-03T07:26:19.427Z",
+      __v: 0,
+    },
+    {
+      _id: "61823991b23540c39ce98f298",
+      user: "618234f53a617ff2cd34e7dd",
+      title: "first note",
+      description: "bro pehlawala hai",
+      tag: "sweet",
+      date: "2021-11-03T07:26:19.427Z",
+      __v: 0,
+    },
+    {
+      _id: "61823199b23540c39ce98f298",
+      user: "618234f53a617ff2cd34e7dd",
+      title: "first note",
+      description: "bro pehlawala hai",
+      tag: "sweet",
+      date: "2021-11-03T07:26:19.427Z",
+      __v: 0,
+    },
+  ];
+  const [notes, setnotes] = useState(notesInitial);
 
+  return <NoteContext.Provider value={{ notes, setnotes }}>{props.children}</NoteContext.Provider>;
+};
 
 export default NoteState;
